273ea909a0f97c8fa07fbd7fbfc523cb74a61354
Sai sangita Adhek <130208503+saisangita05@users.noreply.github.com>
2024-07-27 08:31:13 +0530

    Update ChatListScreen.jsx

--- screens/ChatListScreen.jsx
+++ screens/ChatListScreen.jsx
@@ -1,49 +1,66 @@
-import React from 'react';
-import { StatusBar, View, Text, TouchableOpacity, StyleSheet, FlatList, Image, SafeAreaView } from 'react-native';
+import axios from "axios";
+import React, { useContext, useEffect, useState,useCallback } from "react";
+import { useFocusEffect } from '@react-navigation/native';
+import {
+  StatusBar,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+  Image,
+  SafeAreaView,
+} from "react-native";
+import { AuthContext } from "../AuthContext";
 
 const ChatListScreen = ({ navigation }) => {
-  const chats = [
-    { id: '1', name: 'Luffy', message: 'Kaizoku ni ore na naru', time: '12:45 PM', avatar: 'https://i.pinimg.com/736x/87/69/f8/8769f869b449f860ec1602e2d9c4a524.jpg' },
-    { id: '2', name: 'LavKush', message: 'Chai pio biscuit khaoo', time: '11:30 AM', avatar: 'https://static.wikia.nocookie.net/taarakmehta/images/1/16/D37C4A6A-44EE-4C42-B618-76A4A07030DD.jpeg/revision/latest?cb=20220404185912' },
-    { id: '3', name: 'AntriX', message: 'I am a racist', time: '09:15 AM', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7qFx5FcK4XCfb0_DX7bX-tb1q-nUoG3ncwA&usqp=CAU' },
-    { id: '4', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://wallpaperbat.com/img/116287-lionel-messi-argentina-captain-wallpaper-hd-desktop-and-mobile.jpg' },
-    { id: '5', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://via.placeholder.com/50' },
-    { id: '6', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://via.placeholder.com/50' },
-    { id: '7', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://via.placeholder.com/50' },
-    { id: '8', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://via.placeholder.com/50' },
-    { id: '9', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://via.placeholder.com/50' },
-    { id: '10', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://via.placeholder.com/50' },
-    { id: '11', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://via.placeholder.com/50' },
-    { id: '12', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://via.placeholder.com/50' },
-    { id: '13', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://via.placeholder.com/50' },
-    { id: '14', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://via.placeholder.com/50' },
-    { id: '15', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://via.placeholder.com/50' },
-    { id: '16', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://via.placeholder.com/50' },
-    { id: '17', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://via.placeholder.com/50' },
-    { id: '18', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://via.placeholder.com/50' },
-    { id: '19', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://via.placeholder.com/50' },
-    { id: '20', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://via.placeholder.com/50' },
-    { id: '21', name: 'Messi', message: 'Thanks Grandma', time: '09:15 AM', avatar: 'https://via.placeholder.com/50' },
-    // Add the "Know More" section
-    { id: '22', type: 'knowMore' },
-  ];
+
+
+  const { userId } = useContext(AuthContext);
+  const [chats, setChats] = useState([]);
+  // useEffect(() => {
+  //   if (userId) {
+  //     getUser();
+  //   }
+  // }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      getUser();
+    }, [userId])
+  );
+
+  const getUser = useCallback(async () => {
+    try {
+      const response = await axios.get(
+        `http://192.168.150.145:3000/user/${userId}`
+      );
+      setChats(response.data);
+    } catch (error) {
+      console.log("Error fetching user", error);
+    }
+  },[userId]);
 
   const renderItem = ({ item }) => {
-    if (item.type === 'knowMore') {
-      return (
-        <View style={styles.knowMoreContainer}>
-          <Text style={styles.knowMoreText}>Know More</Text>
-        </View>
-      );
-    }
+    // if (item.type === "knowMore") {
+    //   return (
+    //     <View style={styles.knowMoreContainer}>
+    //       <Text style={styles.knowMoreText}>Know More</Text>
+    //     </View>
+    //   );
+    // }
     return (
-      <TouchableOpacity style={styles.chatContainer} onPress={() => navigation.navigate('chatscreen')}>
-        <Image source={{ uri: item.avatar }} style={styles.avatar} />
+      <TouchableOpacity
+        style={styles.chatContainer}
+        onPress={() => navigation.navigate("chatscreen",{receiverId: item._id,name: item.name,image:item.image})}
+      >
+        <Image source={{ uri: item.image }} style={styles.avatar} />
         <View style={styles.chatDetails}>
           <Text style={styles.chatName}>{item.name}</Text>
-          <Text style={styles.chatMessage}>{item.message}</Text>
+          <Text style={styles.chatMessage}>Hello</Text>
         </View>
-        <Text style={styles.chatTime}>{item.time}</Text>
+        {/* <Text style={styles.chatTime}>{Date.now()}</Text> */}
+        <Text style={styles.chatTime}>2:17 AM</Text>
       </TouchableOpacity>
     );
   };
@@ -51,15 +68,13 @@ const ChatListScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
-        
         <FlatList
           data={chats}
           renderItem={renderItem}
-          keyExtractor={item => item.id}
+          keyExtractor={(item) => item._id}
           style={styles.chatsList}
         />
       </View>
-
     </SafeAreaView>
   );
 };
@@ -68,26 +83,26 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
     // marginTop: StatusBar.currentHeight,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#d6eaff",
   },
   chatsList: {
     padding: 20,
   },
   chatContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     paddingVertical: 10,
     borderBottomWidth: 1,
-    borderBottomColor: '#ECECEC',
+    borderBottomColor: "#ECECEC",
   },
   avatar: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
     marginRight: 10,
   },
   chatDetails: {
@@ -95,32 +110,32 @@ const styles = StyleSheet.create({
   },
   chatName: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   chatMessage: {
     fontSize: 14,
-    color: '#888',
+    color: "#888",
   },
   chatTime: {
     fontSize: 12,
-    color: '#888',
+    color: "#888",
   },
   addIcon: {
     paddingLeft: 30,
     paddingTop: 10,
   },
   knowMoreContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom:20,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
     paddingVertical: 20,
     borderBottomWidth: 1,
-    borderBottomColor: '#ECECEC',
+    borderBottomColor: "#ECECEC",
   },
   knowMoreText: {
     fontSize: 16,
-    fontWeight: 'bold',
-    color: '#007AFF',
+    fontWeight: "bold",
+    color: "#007AFF",
   },
 });
 
