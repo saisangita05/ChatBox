af59cecd3b1c058a735520b3b909a046a6390d9f
Sai sangita Adhek <130208503+saisangita05@users.noreply.github.com>
2024-07-27 08:28:02 +0530

    Update SplashScreen.jsx

--- screens/SplashScreen.jsx
+++ screens/SplashScreen.jsx
@@ -1,12 +1,12 @@
-
-import React, { useEffect } from 'react';
-import { View, StyleSheet, Image, } from 'react-native';
-import * as Animatable from 'react-native-animatable';
+import React, { useEffect } from "react";
+import { View, StyleSheet, Image, Text } from "react-native";
+import * as Animatable from "react-native-animatable";
+import {zoomIn} from "react-native-animatable";
 
 const SplashScreen = ({ navigation }) => {
   useEffect(() => {
     setTimeout(() => {
-      navigation.replace('login');
+      navigation.replace("login");
     }, 4000);
   }, [navigation]);
 
@@ -15,25 +15,30 @@ const SplashScreen = ({ navigation }) => {
       <Animatable.View
         animation={{
           from: {
-            rotate: '0deg',
+            rotate: "0deg",
             scale: 1,
           },
           to: {
-            rotate: '720deg',
+            rotate: "720deg",
             scale: 2,
-          }
+          },
         }}
         duration={3000}
         easing="ease-out"
         iterationCount={1}
         style={styles.logoContainer}
       >
-        <Image
-          source={require('../assets/chatlogo.png')}
-          style={styles.logo}
-        />
+        <Image source={require("../assets/chatlogo1.png")} style={styles.logo} />
       </Animatable.View>
-
+      <Animatable.View style={{ paddingTop: 80 }}
+      animation="zoomIn"
+      duration={3000}
+        easing="ease-out"
+        iterationCount={1}
+      >
+        
+        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 50 }}>CHATBOX</Text>
+      </Animatable.View>
     </View>
   );
 };
@@ -41,18 +46,17 @@ const SplashScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#8de1fc",
   },
   logoContainer: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   logo: {
     width: 100,
     height: 100,
   },
-
 });
 
 export default SplashScreen;
